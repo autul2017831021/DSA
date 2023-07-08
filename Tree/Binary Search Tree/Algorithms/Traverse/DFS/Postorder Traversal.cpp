989123580e153a5bf73772aee69b5be2b689db54
moshiur rahman autul <autul2017831021>
2023-07-08 14:16:10 +0600

    added

--- Tree/Binary Search Tree/Algorithms/Traverse/DFS/Postorder Traversal.cpp
+++ Tree/Binary Search Tree/Algorithms/Traverse/DFS/Postorder Traversal.cpp
@@ -6,3 +6,31 @@ void postorderTraversal(TreeNode* root){
         cout<<root->val<<" ";
     }
 }
+void postorderTraversal(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    stack<TreeNode*> st;
+    TreeNode* prev = nullptr;
+    TreeNode* curr = root;
+
+    while (curr != nullptr || !st.empty()) {
+        if (curr != nullptr) {
+            st.push(curr);
+            curr = curr->left;
+        }
+        else {
+            TreeNode* top = st.top();
+            if (top->right == nullptr || top->right == prev) {
+                st.pop();
+                // Process the node (e.g., print its value)
+                cout << top->val << " ";
+                prev = top;
+            } 
+            else {
+                curr = top->right;
+            }
+        }
+    }
+}
