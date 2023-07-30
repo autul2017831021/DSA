5234bda0f1bd197e055a4ec39be6534d73f7b3ee
moshiur rahman autul <autul2017831021>
2023-07-30 22:11:09 +0600

    solution updated

--- Backtracking/Problem Solving/Leetcode/Combination Sum.cpp
+++ Backtracking/Problem Solving/Leetcode/Combination Sum.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    set<vector<int>>ans;
-    void backtracking(vector<int> v,int target,int i,vector<int>&a){
+    vector<vector<int>>ans;
+    void backtracking(vector<int>&v,int target,int i,vector<int>&a){
         if(target == 0){
             if(a.size()==0)return;
-            ans.insert(a);
+            ans.push_back(a);
+            return;
         }
         if(target<0 || i>=v.size())return;
         backtracking(v,target,i+1,a);
         a.push_back(v[i]);
         backtracking(v,target-v[i],i,a);
-        backtracking(v,target-v[i],i+1,a);
-        if(a.size()>0)a.erase(a.end()-1);
+        a.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& v, int target) {
+        ans.clear();
         vector<int>a;
-        vector<vector<int>>vec;
         backtracking(v,target,0,a);
-        for(auto i:ans){
-            vec.push_back(i);
-        }
-        return vec;
+        return ans;
     }
 };
